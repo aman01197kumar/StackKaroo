895e239b5bfea31931169225bafef2053d56086b
Aman Kumar <aman01197kumar@gmail.com>
2024-01-02 17:39:56 +0530

    fixed images

--- src/app/components/HiringProcessTimeline.tsx
+++ src/app/components/HiringProcessTimeline.tsx
@@ -5,16 +5,17 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { hiringProcessData } from "@/assets/data/hiringProcessData";
+import Image from "next/image";
 
 function HiringProcessTimeline() {
   return (
     <VerticalTimeline>
       {hiringProcessData?.map((item, i) => (
         <VerticalTimelineElement
-          key={i} // Ensure to provide a unique key for each element
+          key={i}
           className="vertical-timeline-element--education"
           iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={item.img_src}
+          icon={<Image src={item.img_src} alt={item.title} />} // Use Next.js Image component instead of img tag
         >
           <h3 className="vertical-timeline-element-title">{item.title}</h3>
           <h4 className="vertical-timeline-element-subtitle">{item.content}</h4>
